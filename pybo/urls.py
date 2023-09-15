f06461f0da648dfd3faf5e52435cac1ee41a58d3
Hanminho <hihi6024@gmail.com>
2023-09-15 17:27:03 +0900

    장고 namespace 사용하기

--- pybo/urls.py
+++ pybo/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from pybo import views
 
+app_name = 'pybo'
+
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
 ]
