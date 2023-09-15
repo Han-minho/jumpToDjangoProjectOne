e8369004b3eb13d130d76d625c334276c4765b90
Hanminho <hihi6024@gmail.com>
2023-09-15 17:19:16 +0900

    django admin 학습

--- pybo/admin.py
+++ pybo/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from pybo.models import Question
+
 # Register your models here.
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['subject']
+    list_display = ['id', 'subject', 'content', 'create_date']
+    list_filter = ['subject']
+    list_per_page = 3
+
+
+admin.site.register(Question, QuestionAdmin)
